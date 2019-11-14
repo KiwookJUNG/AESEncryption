be15c7952b30b2d3820e49bbf4f47567985bb539
KiwookJUNG <ismeeki17@naver.com>
2019-11-14 19:49:38 +0900

    Initial Commit

---
+++ AESEncryption/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AESEncryption
+//
+//  Created by 정기욱 on 2019/11/14.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
